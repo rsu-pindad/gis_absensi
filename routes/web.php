47338accf031c103de6e3bce5343ef3e044c4eb9
rsu-pindad <sautomated01@gmail.com>
2024-08-02 12:53:05 +0700

    move route group auth add route gis,absen,presensi

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+ * |--------------------------------------------------------------------------
+ * | Web Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register web routes for your application. These
+ * | routes are loaded by the RouteServiceProvider and all of them will
+ * | be assigned to the "web" middleware group. Make something great!
+ * |
+ */
 
 Route::view('/', 'welcome');
 
@@ -19,8 +19,18 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::middleware('auth')->group(function () {
+    Route::view('profile', 'profile')
+        ->name('profile');
 
-require __DIR__.'/auth.php';
+    Route::view('gis', 'gis')
+        ->name('gis');
+
+    Route::view('absen', 'absen')
+        ->name('absen');
+
+    Route::view('presensi', 'presensi')
+        ->name('presensi');
+});
+
+require __DIR__ . '/auth.php';
