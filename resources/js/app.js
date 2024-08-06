0920cc301547573d6235954f213add6677a10afb
rsu-pindad <sautomated01@gmail.com>
2024-08-06 16:19:31 +0700

    add laravel sweet alert

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,12 @@ import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import {Html5QrcodeScanner} from "html5-qrcode";
 import {Html5Qrcode} from "html5-qrcode";
-import { detect } from 'un-detector'
+import { detect } from 'un-detector';
+import Swal from 'sweetalert2';
 
 window.mapboxgl = mapboxgl;
 window.MapboxGeocoder = MapboxGeocoder;
 window.Html5QrcodeScanner = Html5QrcodeScanner;
 window.Html5Qrcode = Html5Qrcode;
 window.detect = detect;
+window.Swal = Swal;
