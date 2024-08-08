2b55ecd5e84476ad7fdf53b2031fd890a2e97b69
rsu-pindad <sautomated01@gmail.com>
2024-08-08 11:58:55 +0700

    add meta favicons tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,14 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/apple-touch-icon-180x180.png')}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/favicon-32x32.png')}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/favicon-16x16.png')}}">
+        <link rel="manifest" href="{{asset('/site.webmanifest')}}">
+        <link rel="mask-icon" href="{{asset('/safari-pinned-tab.svg')}}" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#e1ffde">
+        <meta name="theme-color" content="#e1ffde">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -30,12 +38,6 @@
             <!-- Page Content -->
             <main>
                 {{ $slot }}
-
-                @persist('scannerAbsen')
-                @endpersist
-                @persist('mapsAbsen')
-                @endpersist
-
             </main>
         </div>
         @stack('modulejs')
