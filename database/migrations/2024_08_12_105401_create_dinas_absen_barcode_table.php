eed132bacc3297bd20ced2982ce9747204f54e2d
rsu-pindad <sautomated01@gmail.com>
2024-08-12 16:50:01 +0700

    add module dinasbasenbarcode

---
+++ database/migrations/2024_08_12_105401_create_dinas_absen_barcode_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dinas_absen_barcode', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('petugas_id');
+            $table->unsignedBigInteger('absensi_id');
+            $table->string('user_barcode_url');
+            $table->string('user_barcode_img');
+            $table->string('otp_qr')->nullable();
+            $table->string('otp_input')->nullable();
+            $table->text('fingerprint')->nullable();
+            $table->ipAddress('devices_ip')->nullable();
+            $table->json('informasi_device')->nullable();
+            $table->json('informasi_os')->nullable();
+            $table->json('position')->nullable();
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table
+                ->foreign('petugas_id')
+                ->references('id')
+                ->on('users')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table
+                ->foreign('absensi_id')
+                ->references('id')
+                ->on('absensi')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dinas_absen_barcode');
+    }
+};
